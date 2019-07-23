1778662a65695b7de4ec4a33fb069371fc3e5a53
Felddy <markf@geekpad.com>
2019-07-23 17:36:50 -0400

    Add cloud init terraform to render server config template at boot.

---
+++ cloud_init.tf
@@ -0,0 +1,16 @@
+# cloud-init commands for configuring OpenVPN
+
+data "template_cloudinit_config" "cloud_init_tasks" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    filename     = "ci-openvpn-config.yml"
+    content_type = "text/cloud-config"
+    content = templatefile(
+      "${path.module}/cloudinit/ci-openvpn-config.yml.tpl", {
+        private_networks       = var.private_networks
+        client_network         = var.client_network
+    })
+  }
+}
