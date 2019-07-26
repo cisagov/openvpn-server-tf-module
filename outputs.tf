8dfef38ebae63fd3546a631523def0b181b44a56
Felddy <markf@geekpad.com>
2019-07-26 16:06:40 -0400

    Sort outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "id" {
-  value       = aws_instance.openvpn.id
-  description = "The EC2 instance ID"
-}
-
 output "arn" {
   value       = aws_instance.openvpn.arn
   description = "The EC2 instance ARN"
@@ -13,14 +8,14 @@ output "availability_zone" {
   description = "The AZ where the EC2 instance is deployed"
 }
 
+output "id" {
+  value       = aws_instance.openvpn.id
+  description = "The EC2 instance ID"
+}
+
 output "private_ip" {
   value       = aws_instance.openvpn.private_ip
   description = "The private IP of the EC2 instance"
-}
-
-output "subnet_id" {
-  value       = aws_instance.openvpn.subnet_id
-  description = "The ID of the subnet where the EC2 instance is deployed"
 }
 
 output "public_ip" {
@@ -28,12 +23,17 @@ output "public_ip" {
   description = "The public IP of the OpenVPN instance"
 }
 
+output "security_group_arn" {
+  value       = aws_security_group.openvpn_servers.arn
+  description = "The ARN of the OpenVPN server security group"
+}
+
 output "security_group_id" {
   value       = aws_security_group.openvpn_servers.id
   description = "The ID of the OpenVPN server security group"
 }
 
-output "security_group_arn" {
-  value       = aws_security_group.openvpn_servers.arn
-  description = "The ARN of the OpenVPN server security group"
+output "subnet_id" {
+  value       = aws_instance.openvpn.subnet_id
+  description = "The ID of the subnet where the EC2 instance is deployed"
 }
