f8ec6107eeadd9259c197e618134460a2458e228
Felddy <markf@geekpad.com>
2019-08-01 17:21:28 -0400

    Rename files and install additional part.

--- cloud_init.tf
+++ cloud_init.tf
@@ -5,12 +5,21 @@ data "template_cloudinit_config" "cloud_init_tasks" {
   base64_encode = true
 
   part {
-    filename     = "ci-openvpn-config.yml"
+    filename     = "openvpn-config.yml"
     content_type = "text/cloud-config"
     content = templatefile(
-      "${path.module}/cloudinit/ci-openvpn-config.yml.tpl", {
+      "${path.module}/cloudinit/openvpn-config.tpl.yml", {
         private_networks = var.private_networks
         client_network   = var.client_network
+    })
+  }
+
+  part {
+    filename     = "install-certificates.yml"
+    content_type = "text/cloud-config"
+    content = templatefile(
+      "${path.module}/cloudinit/install-certificates.tpl.yml", {
+        server_certificate_arn = aws_acm_certificate.server_cert.arn
     })
   }
 }
