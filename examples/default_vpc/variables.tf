f8482f81e47b113718e64c0455cbfff2f63f988c
Felddy <markf@geekpad.com>
2019-08-03 10:49:58 -0400

    Add variable for cert read role ARN.

--- examples/default_vpc/variables.tf
+++ examples/default_vpc/variables.tf
@@ -1,6 +1,11 @@
-variable "cert_role_arn" {
+variable "cert_create_role_arn" {
   type        = string
-  description = "The ARN of the role that can access the Certificate Manager. (e.g. arn:aws:iam::123456789abc:role/ManageCertificates)"
+  description = "The ARN of the role that can create certs in the Certificate Manager. (e.g. arn:aws:iam::123456789abc:role/ManageCertificates)"
+}
+
+variable "cert_read_role_arn" {
+  type        = string
+  description = "The ARN of the role that can read certs in the Certificate Manager. (e.g. arn:aws:iam::123456789abc:role/ReadCerts)"
 }
 
 variable "dns_role_arn" {
