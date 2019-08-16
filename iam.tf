78fab088304630d2827ce1cdce483e62cd1ff604
Felddy <markf@geekpad.com>
2019-08-15 23:18:26 -0400

    Re-order roles and policies.

--- iam.tf
+++ iam.tf
@@ -1,10 +1,18 @@
 # Create the IAM instance profile for the EC2 server instance
 
+# The profile of the EC2 instance
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "instance_profile"
+  role = aws_iam_role.instance_role.name
+}
+
+# The role for this EC2 instance
 resource "aws_iam_role" "instance_role" {
   name               = "instance_role"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy_doc.json}"
 }
 
+# Attach policies to the instance role
 resource "aws_iam_role_policy" "access_cert_policy" {
   name   = "access_cert_policy"
   role   = aws_iam_role.instance_role.id
@@ -17,16 +25,12 @@ resource "aws_iam_role_policy" "assume_delegated_role_policy" {
   policy = "${data.aws_iam_policy_document.assume_delegated_role_policy_doc.json}"
 }
 
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "instance_profile"
-  role = aws_iam_role.instance_role.name
-}
-
+################################
 # Define the role policies below
+################################
 
 data "aws_iam_policy_document" "assume_role_policy_doc" {
   statement {
-    sid     = "1"
     actions = ["sts:AssumeRole"]
     principals {
       type        = "Service"
@@ -38,7 +42,6 @@ data "aws_iam_policy_document" "assume_role_policy_doc" {
 
 data "aws_iam_policy_document" "read_cert_policy_doc" {
   statement {
-    sid       = "2"
     actions   = ["s3:GetObject"]
     resources = ["${local.cert_bucket_path_arn}"]
     effect    = "Allow"
@@ -47,7 +50,6 @@ data "aws_iam_policy_document" "read_cert_policy_doc" {
 
 data "aws_iam_policy_document" "assume_delegated_role_policy_doc" {
   statement {
-    sid       = "3"
     actions   = ["sts:AssumeRole"]
     resources = ["${var.cert_read_role_arn}"]
     effect    = "Allow"
