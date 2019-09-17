552b2029b77803313232a004a98b3c44a1fd0cd5
Felddy <markf@geekpad.com>
2019-09-17 10:24:30 -0400

    Remove dead code.

--- route53.tf
+++ route53.tf
@@ -15,11 +15,10 @@ resource "aws_route53_record" "server_A" {
 
 resource "aws_route53_record" "server_AAAA" {
   provider = aws.dns
-  #count    = "${aws_instance.openvpn.ipv6_address_count == 0 ? 0 : 1}"
-  count   = "${var.create_AAAA == true ? 1 : 0}"
-  zone_id = data.aws_route53_zone.public_dns_zone.zone_id
-  name    = local.server_fqdn
-  type    = "AAAA"
-  ttl     = var.ttl
-  records = aws_instance.openvpn.ipv6_addresses
+  count    = "${var.create_AAAA == true ? 1 : 0}"
+  zone_id  = data.aws_route53_zone.public_dns_zone.zone_id
+  name     = local.server_fqdn
+  type     = "AAAA"
+  ttl      = var.ttl
+  records  = aws_instance.openvpn.ipv6_addresses
 }
