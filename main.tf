8a4827645785b72ceaf9035fdf286f9429415139
Felddy <markf@geekpad.com>
2019-09-17 12:39:11 -0400

    Add comments to provider usage.

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 # Incoming provider aliases need to be declared
 
+# The "default" provider that is used to do everything else
 provider "aws" {
   alias = "aws"
 }
 
+# The provider that is used to create DNS entries
 provider "aws" {
   alias = "dns"
 }
 
+# The provider that is used to create roles for s3 certificate access
 provider "aws" {
   alias = "cert_read_role"
 }
