c10096c2607edf3b75fd5ca04a4ec6df7e59035b
Felddy <markf@geekpad.com>
2019-10-01 13:35:44 -0400

    Add reverse DNS Zone and records.

--- route53.tf
+++ route53.tf
@@ -22,3 +22,34 @@ resource "aws_route53_record" "server_AAAA" {
   ttl      = var.ttl
   records  = aws_instance.openvpn.ipv6_addresses
 }
+
+#-------------------------------------------------------------------------------
+# Create a private Route53 reverse zone and records.
+#-------------------------------------------------------------------------------
+resource "aws_route53_zone" "private_reverse_zone" {
+  # uses the default tf provider
+  name = "in-addr.arpa."
+
+  vpc {
+    vpc_id = data.aws_vpc.the_vpc.id
+  }
+}
+
+resource "aws_route53_record" "private_PTR" {
+  # uses the default tf provider
+
+  zone_id = aws_route53_zone.private_reverse_zone.zone_id
+  name = format(
+    "%s.%s.%s.%s",
+    element(split(".", aws_instance.openvpn.private_ip), 3),
+    element(split(".", aws_instance.openvpn.private_ip), 2),
+    element(split(".", aws_instance.openvpn.private_ip), 1),
+    element(split(".", aws_instance.openvpn.private_ip), 0),
+  )
+
+  type = "PTR"
+  ttl  = var.ttl
+  records = [
+    "${local.server_fqdn}"
+  ]
+}
