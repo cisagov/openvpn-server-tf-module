7ad57a6ec0a14565ba6321734b749f4525e4ceee
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-27 13:30:40 -0500

    Add comment describing interesting PTR record name behavior
    @dav3r pointed out that this comment (which I shared in the pull
    request) would be good to put in the code as well.

--- route53.tf
+++ route53.tf
@@ -31,6 +31,20 @@ resource "aws_route53_record" "server_AAAA" {
 
 resource "aws_route53_record" "private_PTR" {
   zone_id = var.private_reverse_zone_id
+  # While fixing this I realized that Terraform and/or AWS appears to
+  # append the reverse zone name if you specify just enough of the
+  # record name to "fill in" the rest of the PTR record.  For example,
+  # if this record were for the IP 10.11.12.13, going into the reverse
+  # zone with name "12.11.10.in-addr-arpa", then you could provide the
+  # entire record name ("13.12.11.10.in-addr.arpa.") or just the last
+  # octet ("13").  If you do the latter, then look at the
+  # corresponding Route53 record in the AWS console, you can see that
+  # the ".12.11.10.in-addr.arpa." part of the name has been
+  # automatically added.  With the previous code the record was coming
+  # out as "13.12.11.10.12.11.10.in-addr.arpa.", which is what clued
+  # me into what was happening.
+  #
+  # This allows us to create PTR records more succinctly.
   name = format(
     "%s",
     element(split(".", aws_instance.openvpn.private_ip), 3)
