6bec5080523813396b9ea2c435375201cfa76dc6
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-26 16:40:41 -0400

    Update module inputs to include all required variables.  Override the default value of the ami_owner_account_id input.

--- examples/default_vpc/main.tf
+++ examples/default_vpc/main.tf
@@ -75,14 +75,15 @@ module "example" {
     aws.ssm_read_role  = aws.ssm_read_role
   }
 
+  ami_owner_account_id            = var.ami_owner_account_id
   cert_bucket_name                = var.cert_bucket_name
   cert_read_role_accounts_allowed = var.cert_read_role_accounts_allowed
+  client_dns_search_domain        = "cyber.dhs.gov"
+  client_dns_server               = "10.128.0.2"
   ssm_read_role_accounts_allowed  = var.ssm_read_role_accounts_allowed
   hostname                        = "vpn"
   freeipa_admin_pw                = "thepassword"
   freeipa_realm                   = "COOL.CYBER.DHS.GOV"
-  subdomain                       = "cool"
-  domain                          = "cyber.dhs.gov"
   client_network                  = "10.240.0.0 255.255.255.0"
   private_networks                = ["10.224.0.0 255.240.0.0"]
   private_zone_id                 = aws_route53_zone.private_zone.zone_id
@@ -90,5 +91,7 @@ module "example" {
   security_groups                 = var.security_groups
   subnet_id                       = tolist(data.aws_subnet_ids.default.ids)[0]
   tags                            = { "Name" : "OpenVPN Test" }
-  trusted_cidr_blocks             = ["0.0.0.0/0"]
+  trusted_cidr_blocks_ssh         = ["0.0.0.0/0"]
+  trusted_cidr_blocks_vpn         = ["0.0.0.0/0"]
+  vpn_group                       = "vpnusers"
 }
