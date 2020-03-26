7d47f350b7fa32f5750ebc5bbabdc267cc6341b7
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-03-26 11:41:33 -0400

    Update example code in accordance with input variable changes

--- examples/default_vpc/main.tf
+++ examples/default_vpc/main.tf
@@ -45,8 +45,12 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_route53_zone" "private_reverse_zone" {
+data "aws_route53_zone" "public_zone" {
+  provider = aws.dns
+  name     = "cyber.dhs.gov"
+}
 
+resource "aws_route53_zone" "private_reverse_zone" {
   name = "in-addr.arpa."
 
   vpc {
@@ -82,11 +86,11 @@ module "example" {
   freeipa_admin_pw                = "thepassword"
   freeipa_realm                   = "COOL.CYBER.DHS.GOV"
   subdomain                       = "cool"
-  domain                          = "cyber.dhs.gov"
   client_network                  = "10.240.0.0 255.255.255.0"
   private_networks                = ["10.224.0.0 255.240.0.0"]
   private_zone_id                 = aws_route53_zone.private_zone.zone_id
   private_reverse_zone_id         = aws_route53_zone.private_reverse_zone.zone_id
+  public_zone_id                  = data.aws_route53_zone.public_zone.zone_id
   security_groups                 = var.security_groups
   subnet_id                       = tolist(data.aws_subnet_ids.default.ids)[0]
   tags                            = { "Name" : "OpenVPN Test" }
