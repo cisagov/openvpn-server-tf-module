556650de417e12fc3a769402ddb64145ed502e94
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-21 22:30:08 -0400

    Remove unnecessary provider for resource

--- iam.tf
+++ iam.tf
@@ -55,8 +55,6 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy_attachment" {
 
 # Attach the SSM Agent policy to this role as well
 resource "aws_iam_role_policy_attachment" "ssm_agent_policy_attachment" {
-  provider = aws.provisionassessment
-
   role       = aws_iam_role.instance_role.id
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
