99ac28f00454d2a7ff9669bbb487f64af15127f8
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-08-30 13:15:55 -0400

    Modify code to use an EIP for the OpenVPN server

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,22 @@
+# The EIP for the public IP address of the OpenVPN server
+#
+# Note that the internet gateway has already been created in
+# cool-sharedservices-networking, so the is no need for a depends_on
+# for the IGW here.
+resource "aws_eip" eips {
+  count    = var.associate_public_ip_address ? 1 : 0
+  instance = aws_instance.openvpn.id
+  tags     = var.tags
+  vpc      = true
+}
+
 # The openvpn EC2 instance
 resource "aws_instance" "openvpn" {
-  ami                         = data.aws_ami.openvpn.id
-  ebs_optimized               = true
-  instance_type               = var.aws_instance_type
-  availability_zone           = data.aws_subnet.the_subnet.availability_zone
-  subnet_id                   = var.subnet_id
-  associate_public_ip_address = var.associate_public_ip_address
+  ami               = data.aws_ami.openvpn.id
+  ebs_optimized     = true
+  instance_type     = var.aws_instance_type
+  availability_zone = data.aws_subnet.the_subnet.availability_zone
+  subnet_id         = var.subnet_id
   vpc_security_group_ids = concat([
     aws_security_group.openvpn_servers.id,
   ], var.security_groups)
