ee35662ea350f305caf89e1459ed8373cef67802
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-09-10 11:56:36 -0400

    Add egress on port 80
    This is necessary for Debian updates.

--- security_groups.tf
+++ security_groups.tf
@@ -18,13 +18,17 @@ resource "aws_security_group_rule" "vpn_udp_ingress" {
 
 # TCP egress rules for OpenVPN
 #
+# Egress on port 80 is necessary for Debian updates.
+#
 # Egress on port 443 is needed for AWS API commands (e.g. AssumeRole,
 # which is needed in order to fetch the server certificate from S3).
 resource "aws_security_group_rule" "openvpn_tcp_https_egress" {
+  for_each = toset(["80", "443"])
+
   security_group_id = aws_security_group.openvpn_servers.id
   type              = "egress"
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 443
-  to_port           = 443
+  from_port         = each.value
+  to_port           = each.value
 }
