c56f54077d1d8ed352e212d3288bd142468441d7
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-05-08 15:06:32 -0400

    Add nullable to all variables

--- examples/basic_usage/variables.tf
+++ examples/basic_usage/variables.tf
@@ -6,31 +6,37 @@
 
 variable "cert_bucket_name" {
   description = "The name of the bucket that stores the certificates (e.g. my-certificates)."
+  nullable    = false
   type        = string
 }
 
 variable "cert_read_role_arn" {
   description = "The ARN of the role that can create roles to have read access to the S3 bucket ('cert_bucket_name' above) where certificates are stored."
+  nullable    = false
   type        = string
 }
 
 variable "dns_role_arn" {
   description = "The ARN of the role that can modify route53 DNS (e.g. arn:aws:iam::123456789abc:role/ModifyPublicDNS)."
+  nullable    = false
   type        = string
 }
 
 variable "freeipa_domain" {
   description = "The domain for the IPA client (e.g. example.com)."
+  nullable    = false
   type        = string
 }
 
 variable "public_dns_zone_id" {
   description = "The DNS zone ID in which to create public lookup records."
+  nullable    = false
   type        = string
 }
 
 variable "ssm_read_role_arn" {
   description = "The ARN of the role that can create roles to have read access to the SSM parameters."
+  nullable    = false
   type        = string
 }
 
@@ -63,17 +69,20 @@ variable "aws_region" {
 variable "cert_read_role_accounts_allowed" {
   default     = []
   description = "A list of accounts allowed to access the role that can read certificates from an S3 bucket."
+  nullable    = false
   type        = list(string)
 }
 
 variable "security_groups" {
   default     = []
   description = "Additional security group ids the server will join."
+  nullable    = false
   type        = list(string)
 }
 
 variable "ssm_read_role_accounts_allowed" {
   default     = []
   description = "A list of accounts allowed to access the role that can read SSM keys."
+  nullable    = false
   type        = list(string)
 }
